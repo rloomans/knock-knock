6acae28a9da2af873c944784ee45b65683aeb5bf
Robert Loomans <robert@loomans.org>
2019-03-15 21:59:52 +1000

    triangle test

---
+++ KnockKnockApi/Library/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnockKnockApi.Library
+{
+    public class Triangle
+    {
+        public int a;
+        public int b;
+        public int c;
+
+        public string Characterise()
+        {
+            if (a == b && b == c)
+            {
+                return "Equilateral";
+            }
+            else if (a == b || b == c || c == a)
+            {
+                return "Isosceles";
+            }
+
+            return "Scalene";
+        }
+    }
+}
---
+++ KnockKnockApiTests/TriangleTypeTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using FluentAssertions;
+using KnockKnockApi.Library;
+
+namespace KnockKnockApiTests
+{
+    public class TriangleTypeTests
+    {
+        [Theory]
+        [InlineData(4, 4, 4, "Equilateral")]
+        [InlineData(4, 4, 5, "Isosceles")]
+        [InlineData(3, 4, 5, "Scalene")]
+        public void CheckTriangleType(int a, int b, int c, string expected_result)
+        {
+            // Arrange
+            var triangle = new Triangle() { a = a, b = b, c = c };
+            // Act
+            var result = triangle.Characterise();
+
+            // Assert
+            result.Should().Be(expected_result);
+        }
+    }
+}
