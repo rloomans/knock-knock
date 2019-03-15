95a0ac06c28daace573c818c7f1f46d0902f1dd6
Robert Loomans <robert@loomans.org>
2019-03-16 01:00:11 +1000

    Cache fibonacci numbers

--- KnockKnockApi/Library/Fibonacci.cs
+++ KnockKnockApi/Library/Fibonacci.cs
@@ -1,15 +1,26 @@
-﻿namespace KnockKnockApi.Library
+﻿using System.Collections.Generic;
+
+namespace KnockKnockApi.Library
 {
     public class Fibonacci
     {
+        private static Dictionary<long, long> _cache = new Dictionary<long, long>();
+
         public static long Calculate(long n)
         {
             if (n == 0) return 0;
             if (n == 1) return 1;
 
-            if (n < 0) return -Calculate(-n);
+            if (n < 0) return (-1 * Calculate(-n));
 
-            return Calculate(n - 1) + Calculate(n - 2);
+            long result;
+
+            if (_cache.TryGetValue(n, out result))
+                return result;
+
+            result = Calculate(n - 1) + Calculate(n - 2);
+            _cache.Add(n, result);
+            return result;
         }
     }
 }
